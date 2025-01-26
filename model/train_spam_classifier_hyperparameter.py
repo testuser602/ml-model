9d6509581298712e4079ca42c6ddbb2df2fdc7ec
Dharini G <dharini.g@gendigital.com>
2025-01-27 00:40:10 +0530

    updated changes

--- model/train_spam_classifier_hyperparameter.py
+++ model/train_spam_classifier_hyperparameter.py
@@ -6,7 +6,7 @@ from sklearn.metrics import accuracy_score
 import pickle
 
 # Load dataset
-df = pd.read_csv("SMSSpamCollection", sep="\t", names=["label", "message"])
+df = pd.read_csv("./data/SMSSpamCollection", sep="\t", names=["label", "message"])
 
 # Convert labels to binary (spam = 1, ham = 0)
 df['label'] = df['label'].map({'ham': 0, 'spam': 1})
